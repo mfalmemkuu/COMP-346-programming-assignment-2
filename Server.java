cc1d0bc5c3ce44f8a9e6a22684d7732aa144abb0
mfalmemkuu <78621540+mfalmemkuu@users.noreply.github.com>
2023-03-10 17:10:35 -0500

    Add files via upload

--- Server.java
+++ Server.java
@@ -341,7 +341,8 @@ public class Server extends Thread {
    
      public double deposit(int i, double amount)
      {  double curBalance;      /* Current account balance */
-       
+       synchronized(this)
+       {
      		curBalance = account[i].getBalance( );          /* Get current account balance */
         
      		/* NEW : A server thread is blocked before updating the 10th , 20th, ... 70th account balance in order to simulate an inconsistency situation */
@@ -358,6 +359,7 @@ public class Server extends Thread {
      		System.out.println("\n DEBUG : Server.deposit - " + "i " + i + " Current balance " + curBalance + " Amount " + amount + " " + getServerThreadId());
         
      		account[i].setBalance(curBalance + amount);     /* Deposit amount in the account */
+       }
      		return account[i].getBalance ();                /* Return updated account balance */
      }
          
@@ -371,11 +373,13 @@ public class Server extends Thread {
      public double withdraw(int i, double amount)
      {  double curBalance;      /* Current account balance */
         
+     synchronized(this) {
      	curBalance = account[i].getBalance( );          /* Get current account balance */
           
         System.out.println("\n DEBUG : Server.withdraw - " + "i " + i + " Current balance " + curBalance + " Amount " + amount + " " + getServerThreadId());
         
         account[i].setBalance(curBalance - amount);     /* Withdraw amount in the account */
+     }
         return account[i].getBalance ();                /* Return updated account balance */
      	
      }
@@ -389,11 +393,12 @@ public class Server extends Thread {
  
      public double query(int i)
      {  double curBalance;      /* Current account balance */
-        
+        synchronized(this)
+        {
      	curBalance = account[i].getBalance( );          /* Get current account balance */
         
         System.out.println("\n DEBUG : Server.query - " + "i " + i + " Current balance " + curBalance + " " + getServerThreadId()); 
-        
+        }
         return curBalance;                              /* Return current account balance */
      }
          
